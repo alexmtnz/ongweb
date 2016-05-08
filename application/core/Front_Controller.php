8c7e3e7b32b76185661984c420295471658d9f1a
Issam <issinatour90@gmail.com>
2016-05-08 17:08:13 +0200

    default

---
+++ application/core/Front_Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Issam
+ * Date: 08/05/2016
+ * Time: 16:43
+ */
+class Front_Controller extends MY_Controller {
+
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+include("Front_Controller.php");
+
+class MY_Controller extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+
+    }
+}
